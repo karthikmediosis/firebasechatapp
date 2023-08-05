924985c2012b92b657ff2a1d1e315e8220a1b047
ajith kumar <ajithanandan2306@gmail.com>
2023-08-05 14:54:15 +0530

    logout done

--- app/Component/Header/HomeHeader.js
+++ app/Component/Header/HomeHeader.js
@@ -2,12 +2,25 @@ import { Icon } from "native-base";
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { Avatar } from "react-native-elements/dist/avatar/Avatar";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { COLORS } from "../Constant/Color";
 import { FONTS } from "../Constant/Font";
+import AntDesign from "react-native-vector-icons/AntDesign";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Auth from "../../Service/Auth";
+import { removeUser } from "../../Redux/reducer/user";
+import Navigation from "../../Service/Navigation";
 
 const HomeHeader = () => {
   const { userData } = useSelector((state) => state.User);
+  const dispatch = useDispatch();
+
+  //handleToLogout
+  const handleToLogout = async () => {
+    dispatch(removeUser());
+    await Auth.logout();
+    Navigation.navigate("Login");
+  };
   return (
     <View
       style={{
@@ -23,12 +36,9 @@ const HomeHeader = () => {
     >
       <Text style={styles.logo}>FireBase Chat App</Text>
       <View style={{ flexDirection: "row", alignItems: "center" }}>
-        <Icon
-          name="notifications"
-          type="Ionicons"
-          style={{ color: COLORS.theme, marginRight: 7 }}
-        />
-        <Avatar source={{ uri: userData.img }} rounded size="small" />
+        <TouchableOpacity style={styles.but} onPress={handleToLogout}>
+          <AntDesign name="logout" size={20} color={COLORS.green} />
+        </TouchableOpacity>
       </View>
     </View>
   );
